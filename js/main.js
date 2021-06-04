49f7558d43fe8fce129c2f3080af9414b453ed02
David Bercovici <bercovici.david@gmail.com>
2021-06-03 20:28:02 -0500

    nav setup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// navigation menu
+const navbar = document.querySelector('.navbar-open')
+
+function closeNav() {
+  console.log('clicked');
+ navbar.classList.add('navbar-close');
+ navbar.classList.remove('navbar-open');
+}
+
+document.getElementById('cross').addEventListener('click', closeNav);
